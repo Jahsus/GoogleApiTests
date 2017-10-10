3ff7778889c048529d3df7b38d8d8d92ddc9344c
Valentin <vk@u-s.su>
2017-10-10 12:18:55 +0300

    Added 1st test run

---
+++ src/test/java/ivi/tests/models/NearbyResponse.java
@@ -0,0 +1,13 @@
+package ivi.tests.models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by vk on 10.10.17.
+ */
+public class Response {
+
+
+    @SerializedName("results")
+
+}
--- src/test/java/ivi/tests/models/Result.java
+++ src/test/java/ivi/tests/models/Result.java
@@ -3,5 +3,5 @@ package ivi.tests.models;
 /**
  * Created by vk on 10.10.17.
  */
-public class Viewport {
+public class Result {
 }
